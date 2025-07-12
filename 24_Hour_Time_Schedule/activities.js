633c6427f8a68fefa0bbd318a5ecc2d1fae768e4
Subham <163439445+Subhamahato@users.noreply.github.com>
2025-07-12 19:40:03 +0530

    Create activities.js

---
+++ 24_Hour_Time_Schedule/activities.js
@@ -0,0 +1,13 @@
+const activities = [
+  { label: "🛌Sleep", from: 22, to: 4, color: "gray" },
+  { label: "📗Diary Writing", from: 4, to: 5, color: "green" },
+  { label: "🛀Fresh", from: 5, to: 6, color: "blue" },
+  { label: "🍚Breakfast", from: 6, to: 7, color: "yellow" },
+  { label: "🏍️Out for College", from: 7, to: 8.25, color: "brown" },
+  { label: "🏫College", from: 8.25, to: 16.75, color: "cyan" },
+  { label: "🍱Lunch/Break", from: 13.50, to: 14, color: "yellow" },
+  { label: "🏍️Out from College", from: 16.75, to: 18, color: "brown" },
+  { label: "Free Time", from: 18, to: 20, color: "pink" },
+  { label: "🍛Dinner", from: 20, to: 21, color: "yellow" },
+  { label: "Relax/YouTube", from: 21, to: 22, color: "purple" }
+];
